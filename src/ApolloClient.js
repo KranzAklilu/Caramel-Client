ad4fd5885987c2061462945b0f710a1bf686233a
KranzAklilu <kranz.aklilu@gmail.com>
2020-12-06 23:27:08 +0300

    Added proxy and edited url

--- src/ApolloClient.js
+++ src/ApolloClient.js
@@ -2,7 +2,7 @@ import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
 import { setContext } from "apollo-link-context";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:5000",
+  uri: "https://fathomless-sierra-24501.herokuapp.com/",
 });
 
 const authLink = setContext(() => {
